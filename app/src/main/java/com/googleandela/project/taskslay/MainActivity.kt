1232e5d780572323aa0db6c5f8a70199ec6bed45
bright <brightahedor@gmail.com>
2020-10-15 19:52:51 +0100

    added fonts

---
+++ app/src/main/java/com/googleandela/project/taskslay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.googleandela.project.taskslay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
