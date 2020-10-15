7908075cbe87ef58340012dd2b40b69b0df86da7
bright <brightahedor@gmail.com>
2020-10-15 19:59:18 +0100

    added splash activity

---
+++ app/src/main/java/com/googleandela/project/taskslay/startup/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.googleandela.project.taskslay.startup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.googleandela.project.taskslay.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
